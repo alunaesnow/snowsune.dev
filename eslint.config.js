173a171972c3cbe94682cc1971772f3f708a4dcc
Hope <59150607+alunaesnow@users.noreply.github.com>
2026-04-29 12:54:22 +0100

    style: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -39,6 +39,19 @@ export default defineConfig(
 	{
 		// Override or add rule settings here, such as:
 		// 'svelte/button-has-type': 'error'
-		rules: {}
+		rules: {
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					args: 'all',
+					argsIgnorePattern: '^_',
+					caughtErrors: 'all',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+					ignoreRestSiblings: true
+				}
+			]
+		}
 	}
 );
